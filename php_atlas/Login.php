a130d793f9d069ade127b7a4e2803beef5ab512d
Gabriel Reis <147285271+GabrieldsReis@users.noreply.github.com>
2024-09-10 20:45:28 -0300

    Add files via upload

---
+++ php_atlas/Login.php
@@ -0,0 +1,24 @@
+<?php
+$username = "root";
+$servername = "localhost"
+$dbname = "tcc_atlas"
+$password = ""
+
+$conn = new mysqli($servername,$username,$passoword,$dbname);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
