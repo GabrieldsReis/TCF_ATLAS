2aaa42ebb0c639ff9462b1b93836fa6706f7133c
Gabriel Reis <147285271+GabrieldsReis@users.noreply.github.com>
2024-09-24 20:43:26 -0300

    Add files via upload

--- php_atlas/Cadastro.php
+++ php_atlas/Cadastro.php
@@ -5,8 +5,10 @@ $servername = "localhost";
 $dbname = "tcc_atlas";
 $password = "";
 
+//caminhos do php
 
 echo var_dump($_POST);
+//mostra os dados do usuario
 
 $conn = new mysqli($servername,$username,$password,$dbname);
 
@@ -15,11 +17,14 @@ if ($conn->connect_error){
     die("Falha ao conectar " . $conn -> connect_error);
 }
 
+// se houver conexao ao sevidor
+
 $nome = $_POST["nome"];
 $idade = $_POST["idade"];
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
+//salvar os dados as variaveis
 
 $cmdSql = "
 INSERT INTO usuarios(Nome,Idade,Email,Senha)
@@ -29,7 +34,7 @@ echo $cmdSql;
 if($conn -> query($cmdSql) === TRUE){
     echo("Cadastro Concluido!");
 } else{ 
-    echo("Erro no cadastro em " . $conn->error);
+    echo("Erro no cadastro em " . $conn->error); //motivo do erro
 
 }
 
--- php_atlas/Login.php
+++ php_atlas/Login.php
@@ -1,24 +1,46 @@
 <?php
-$username = "root";
-$servername = "localhost"
-$dbname = "tcc_atlas"
-$password = ""
 
-$conn = new mysqli($servername,$username,$passoword,$dbname);
-
-
-
+header("Access-Control-Allow-Origin:*");
+//Tag para iniciar o codigo php
+$username = "root";  //variavel que sera o usuario do banco de dados
+$servername = "localhost"; //endereco url do database
+$dbname = "tcc_atlas"; //nome do banco de dados
+$password = ""; //senha do banco de dados
 
 
 
+$conn = new mysqli($servername,$username,$password,$dbname); //um caminho pra acessar o banco de dados
+
+if($conn->connect_error){
+    die("Falha ao conectar".$conn -> connect_error);
+}// caso nao consiga acessar o banco de dados,ira apresentar o erro
+
+$email = $_POST["email"];//salva o email e senha em variaveis
+$senha = $_POST["senha"];
+
+$Login =  "SELECT * FROM usuarios WHERE Email = '$email' AND Senha = '$senha'" ; 
+//comando sql para buscar email e senha salvos na variavel
+$resultado = $conn -> query($Login);
+// echo($resultado->num_rows);
+
+
+if($resultado->num_rows >0){
+
+die("logado");
+
+}
+
+    else{
+die("deslogado");
+
+    }
+
+
+// while ($row = $resultado->fetch_assoc()) {
+//     echo $row['Nome']."<br>";
+// }
 
 
 
-
-
-
-
-
-
-
+$conn->close();//fecha a conexao
 ?>
