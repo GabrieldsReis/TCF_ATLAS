58ca71a10d9797300f8fffc7cdbcc2b329fec06e
Gabriel Reis <147285271+GabrieldsReis@users.noreply.github.com>
2024-12-17 20:09:07 -0300

    Add files via upload

--- src/paginas/Principal.js
+++ src/paginas/Principal.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './Principal.css';
+import './Principal.css'; // Verifique se a folha de estilo está importada corretamente
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,9 +7,17 @@ function Principal() {
   // Estado para controlar a visibilidade do formulário
   const [showPostForm, setShowPostForm] = useState(false);
 
+  // Estado para controlar a visibilidade da aba lateral
+  const [isAbaLateralVisible, setIsAbaLateralVisible] = useState(false);
+
   // Função para alternar a visibilidade do formulário
   const togglePostForm = () => {
     setShowPostForm(prevState => !prevState);
+  };
+
+  // Função para alternar a visibilidade da aba lateral
+  const toggleAbaLateral = () => {
+    setIsAbaLateralVisible(prevState => !prevState);
   };
 
   // Função que processa os posts
@@ -48,8 +56,10 @@ function Principal() {
   return (
     <div id="site">
       {/* Menu Superior */}
-      <div className="menu-superior">
-        <i className="fa-brands fa-instagram logo"></i>
+      <div href="#" className="menu-superior">
+        <button id="toggle-aba" onClick={toggleAbaLateral}>
+          {isAbaLateralVisible ? '#' : '#'}
+        </button>
         <div id="perfis-pai">
           {/* Imagens de perfis */}
           <img className="perfis" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Perfil 1" />
@@ -60,59 +70,54 @@ function Principal() {
         </div>
       </div>
 
-      {/* Container Principal */}
-      <div className="container">
-        {/* Menu Lateral */}
-        <div className="aba-lateral">
-          <div className="menu-lateral">
-            <a id="imgs" href="#">🏠</a>
-            <a id="imgs" href="#">🔍</a>
-            <a id="imgs" href="#">❤️</a>
-            <a id="imgs" href="#">👤</a>
-            <a id="imgs" href="#">⚙️</a>
-          </div>
+      {/* Menu Lateral */}
+      <div className={`aba-lateral ${isAbaLateralVisible ? 'show' : 'hide'}`}>
+        <div className="menu-lateral">
+          <a id="imgs" href="#">🏠 Home</a>
+          <a id="imgs" href="#">🔍 Busca</a>
+          <a id="imgs" href="#">❤️ Favoritos</a>
+          <a id="imgs" href="#">👤 Perfil</a>
+          <a id="imgs" href="#">⚙️ Configurações</a>
         </div>
+      </div>
 
-        
+      {/* Feed de Postagens */}
+      <div id="feed" className="feed">
+        {/* Botão para exibir ou esconder o formulário */}
+        <button id="tt" onClick={togglePostForm}>
+          {showPostForm ? 'voltar' : 'Postar'}
+        </button>
 
-        {/* Feed de Postagens */}
-        <div id="feed" className="feed">
-          {/* Botão para exibir ou esconder o formulário */}
-          <button id="tt"onClick={togglePostForm}>
-            {showPostForm ? 'voltar' : 'Postar'}
-          </button>
-
-          {/* Formulário de Postagem */}
-          {showPostForm && (
-
-            <form id="postar" action="http://localhost/Gabriel-%20Atlas/php_atlas/Postagens.php" method="POST" encType="multipart/form-data">
-            <div style={{ display: 'flex' }}>          
-              <img id="usuario11" className="post-profile"  src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post profile" />
-              <input placeholder="O que está acontecendo?" type="text" id="fname" name="texto" /></div>
-
-              <input type="file" name="fileUpload" />
-              <br />
-              <button id="obotao" name="arquivo" type="submit">Postar</button>
-            </form>
-          )}
-
-          {/* Postagens */}
-          <div id="Inicio" className="postagem">
-            <div className="post-header">
-              <img className="post-profile" id="Oteste" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post profile" />
-              <span className="post-username" id='test1'>Usuário 1</span>
+        {/* Formulário de Postagem */}
+        {showPostForm && (
+          <form id="postar" action="http://localhost/Gabriel-%20Atlas/php_atlas/Postagens.php" method="POST" encType="multipart/form-data">
+            <div style={{ display: 'flex' }}>
+              <img id="usuario11" className="post-profile" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post profile" />
+              <input placeholder="O que está acontecendo?" type="text" id="fname" name="texto" />
             </div>
-            <img className="imagem-postagem" id="ImagemPost" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post 1" />
-            <p className="descricao-postagem">Descrição da postagem aqui</p>
+
+            <input type="file" name="fileUpload" />
+            <br />
+            <button id="obotao" name="arquivo" type="submit">Postar</button>
+          </form>
+        )}
+
+        {/* Postagens */}
+        <div id="Inicio" className="postagem">
+          <div className="post-header">
+            <img className="post-profile" id="Oteste" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post profile" />
+            <span className="post-username" id='test1'>Usuário 1</span>
           </div>
-          <div id="Inicio" className="postagem">
-            <div className="post-header">
-              <img className="post-profile" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post profile" />
-              <span className="post-username">Usuário 2</span>
-            </div>
-            <img className="imagem-postagem" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post 2" />
-            <p className="descricao-postagem">Outra descrição aqui</p>
+          <img className="imagem-postagem" id="ImagemPost" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post 1" />
+          <p className="descricao-postagem">Descrição da postagem aqui</p>
+        </div>
+        <div id="Inicio" className="postagem">
+          <div className="post-header">
+            <img className="post-profile" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post profile" />
+            <span className="post-username">Usuário 2</span>
           </div>
+          <img className="imagem-postagem" src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/17/1250579066-i764852.png" alt="Post 2" />
+          <p className="descricao-postagem">Outra descrição aqui</p>
         </div>
       </div>
     </div>
